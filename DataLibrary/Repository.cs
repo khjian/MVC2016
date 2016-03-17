4b8614a9f871ea7f0dfbd833471d33f520044e75
songshujian <kuaihujian@qq.com>
2016-03-17 18:40:54 +0800

    增加Repository类

---
+++ DataLibrary/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataLibrary
+{
+    class Repository
+    {
+        public DbContext DbContext { get; set; }
+        public Repository()
+        { }
+
+        public Repository(DbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        /// <summary>
+        /// 查找实体
+        /// </summary>
+        /// <param name="ID">实体主键值</param>
+        /// <returns></returns>
+        public T Find(int ID)
+        {
+            return DbContext.Set<T>().Find(ID);
+        }
+
+        /// <summary>
+        /// 查找实体
+        /// </summary>
+        /// <param name="where">查询Lambda表达式</param>
+        /// <returns></returns>
+        public T Find(Expression<Func<T, bool>> where)
+        {
+            return DbContext.Set<T>().SingleOrDefault(where);
+        }
+    }
+}
